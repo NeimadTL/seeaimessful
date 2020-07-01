458215e0ef48e3306cd7ffc35d9cd25579bf5f67
neimad971 <damientalbot26@gmail.com>
2020-07-01 20:44:22 +0200

    makes sure user and user_*_input are valid and present in user_*_value
     - adds validations and spec for those validations in user_*_value

--- app/models/user_sign_up_form_input_value.rb
+++ app/models/user_sign_up_form_input_value.rb
@@ -1,5 +1,8 @@
 class UserSignUpFormInputValue < ApplicationRecord
 
+  validates_presence_of :user_sign_up_form_input
+  validates_presence_of :user
+  
   belongs_to :user_sign_up_form_input
   belongs_to :user
 
--- spec/models/user_sign_up_form_input_value_spec.rb
+++ spec/models/user_sign_up_form_input_value_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe UserSignUpFormInputValue, type: :model do
 
+  it { should validate_presence_of :user_sign_up_form_input }
+  it { should validate_presence_of :user }
+
   it { should belong_to :user_sign_up_form_input }
   it { should belong_to :user }
-  
+
 end
